d6cfc1869d871b1f7b5f8ed22c219cca28b64cb9
MrtkGG <49731640+MrtkGG@users.noreply.github.com>
2020-02-01 13:43:24 +0300

    Add files via upload

--- src/Selam/Main.php
+++ src/Selam/Main.php
@@ -1,1 +1,36 @@
+<?php
 
+namespace Selam;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\Listener;
+use pocketmine\{Player, Server};
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+/**
+ * @param \pocketmine\event\player\PlayerChatEvent $event
+ * @priority LOWEST
+ */
+
+    public function onChat(PlayerChatEvent $e){
+		$mesaj = strtolower($e->getMessage());
+		$tag = null;
+		if (substr($mesaj,0,1) == "!"){
+			$mesaj = substr($mesaj,1);
+			$tag = "!";
+		}
+		if ($mesaj == "sa"){
+			$e->setMessage($tag . "Selamun Aleyküm");
+		}elseif ($mesaj == "as"){
+			$e->setMessage($tag . "Aleyküm Selam");
+		}
+	}
+}
+
+?>
